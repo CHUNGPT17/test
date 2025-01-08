786a39449f96d32ff294e95263cc3cc364059d53
CHUNGSEV <116159227+CHUNGSEV@users.noreply.github.com>
2025-01-08 08:51:30 +0700

    Update mac80211.sh

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -200,7 +200,7 @@ detect_mac80211() {
 			set wireless.${name}=wifi-device
 			set wireless.${name}.type=mac80211
 			${dev_id}
-			set wireless.${name}.channel=auto
+			set wireless.${name}.channel=${channel}
 			set wireless.${name}.band=${mode_band}
 			set wireless.${name}.htmode=$htmode
 			set wireless.${name}.disabled=0
